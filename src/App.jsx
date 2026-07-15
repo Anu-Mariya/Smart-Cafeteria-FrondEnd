a9a7a7493c6a3ef12814065d0e242a0abc3cd808
Sanjaysna <sanjaysanthosh411@gmail.com>
2026-07-15 11:28:17 +0530

    feature add view menu items

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddMenuitem from './components/AddMenuitem'
+import ViewMenuitems from './components/ViewMenuitems'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <AddMenuitem />
+    <ViewMenuitems />
       
     </>
   )
